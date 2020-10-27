36c89c868d7c7eee3a4cd65e7dd3f6d9c8a48803
x0r19x91 <x0r19x91@outlook.com>
2020-10-27 17:05:16 +0530

    make unpackScript private

--- autoit.go
+++ autoit.go
@@ -95,7 +95,7 @@ func GetScripts(data []byte) (*AutoItFile, error) {
     }
     for start, end := range possibleScripts {
         script := data[start:end]
-        res, err := UnpackScript(script, isLegacy, file.Version)
+        res, err := unpackScript(script, isLegacy, file.Version)
         if err != nil {
             return file, err
         }
@@ -105,7 +105,7 @@ func GetScripts(data []byte) (*AutoItFile, error) {
     return file, nil
 }
 
-func UnpackScript(script []byte, bLegacy bool, ver AutoItVersion) ([]*AutoItResource, error) {
+func unpackScript(script []byte, bLegacy bool, ver AutoItVersion) ([]*AutoItResource, error) {
     var iKeys IKeySet
     if ver == EA06 {
         iKeys = NewEA06()
